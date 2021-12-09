e42c05480f23570d86c7f7a0c1f02188242b908c
Arthur Brasa <arthurbrasa@outlook.com>
2021-12-09 19:49:03 -0300

    desenvolvendo class crud.php

---
+++ app_lista_tarefas/crud.php
@@ -0,0 +1,26 @@
+<?php 
+	
+	class ControllerListaTarefa {
+		private $tarefa;
+		private $bd;
+
+		public function cread() {
+			// ...
+		}
+
+		public function read() {
+			// ...
+		}
+
+		public function update() {
+			// ...
+		}
+
+		public function delete() {
+			// ...
+		}
+
+	}
+
+
+?>
