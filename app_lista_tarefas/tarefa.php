37304debf4d23803a959ba21028176461f108d3b
Arthur Brasa <arthurbrasa@outlook.com>
2021-12-09 20:46:57 -0300

    Criacao de get/set tarefa.php

--- app_lista_tarefas/tarefa.php
+++ app_lista_tarefas/tarefa.php
@@ -1,5 +1,24 @@
 <?php 
+	
+	class Tarefa {
+		private $tarefa;
+	
+
+		public function __set($att, $value) {
+			$this->$att = $value;
+		}
+
+		public function __get($value) {
+			return $this->$value;
+		}
+
+	}
 
 
+	// teste
+	/*
+	$teste = new Tarefa();
+	$teste->__set('tarefa', 'Estudar concurso do Ibama');
+	echo $teste->__get('tarefa'); */
 
 ?>
