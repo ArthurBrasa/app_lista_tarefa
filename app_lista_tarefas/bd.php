a7445915f558b2408322e7fbd8c8b9bba1960d02
Arthur Brasa <arthurbrasa@outlook.com>
2021-12-09 20:45:00 -0300

    Criação de bd.php

---
+++ app_lista_tarefas/bd.php
@@ -0,0 +1,42 @@
+<?php  
+
+	/**
+	 * Class para iniciação do banco de dados
+	 */
+	class BdConection
+	{
+		private $root;
+		private $pass;
+		private $dns;
+
+		public function __construct($dns, $root, $pass)
+		{
+			$this->root = $root;
+			$this->pass = $pass;
+			$this->dns = $dns;
+		}
+
+		public function conecte() {
+			try
+			{
+				// code...
+				$connection = new PDO($this->dns, $this->root, $this->pass);
+				return $connection;
+			}
+			catch(PDOException $e)
+			{
+				// code...
+
+				return "Código do erro: ".$e->getCode()." Mensagem: ".$e->getMessage();
+			}
+		}
+
+
+	}
+
+	/*
+	$bd = new BdConection('mysql:host=localhost;bdname=lista_tarefa', 'root', '');	
+	$stmt = $bd->conecte();
+	print_r($stmt) */
+
+?>
